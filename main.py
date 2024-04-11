75d06a3b8544fdd03a211b14fd83a7f96c444c54
AnvitD <anvit.dhamnekar@gmail.com>
2024-04-11 15:20:49 -0400

    My code for the lab

---
+++ main.py
@@ -0,0 +1,35 @@
+def encode(password):
+    encoded = []
+    for i in range(len(password)):
+        encoded.append(chr(ord(password[i]) + 3))
+
+    return "".join(encoded)
+
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+def main():
+
+    while True:
+        menu()
+
+        choice = input("Please enter an option: ")
+
+        global enc
+
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            enc = encode(password)
+            print("Your password has been encoded and stored!")
+        elif choice == "2":
+            print(f"The encoded password is {enc}, and the original password is {}")
+        elif choice == "3":
+            break
+        else:
+            print("Wrong input")
+
+main()
